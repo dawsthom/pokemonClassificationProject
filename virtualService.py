e31389780ca998e73759309acf8c15c02d70f221
Dawson Thompson <thompsondawson20@gmail.com>
2021-04-01 22:05:53 -0400

    modified code to run pokemon classification service

---
+++ virtualService.py
@@ -0,0 +1,14 @@
+from sklearn import datasets
+from joblib import load
+import numpy as np
+import json
+
+#load the model
+
+savedModel = load('pkmnClassifierModel.pkl')
+
+def classify(id):
+    inputData = np.array(id)
+    inputDataReshaped = inputData.reshape(1, -1)
+    prediction = savedModel.predict(inputDataReshaped)
+    return prediction
